5afcae1be120a8b80c474cb5fcdf4f2b96bccedf
TDHouston <trentdhouston@gmail.com>
2023-07-18 19:27:15 -0400

    intro useState added

--- imr-react/src/Box.js
+++ imr-react/src/Box.js
@@ -1,8 +1,28 @@
-export function Box() {
+import { useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+
+export function Box(props) {
+  const ref = useRef()
+  const [hovered, setHover] = useState(false)
+  const [rotate, setRotate] = useState(false)
+
+  useFrame((_, delta) => {
+    if (rotate) {
+      ref.current.rotation.x += 1 * delta
+      ref.current.rotation.y += 0.5 * delta
+    }
+  })
+
   return (
-    <mesh>
+    <mesh
+      {...props}
+      ref={ref}
+      scale={hovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
+      onPointerDown={() => setRotate(!rotate)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}>
       <boxGeometry />
-      <meshBasicMaterial color={0x00ff00} wireframe />
+      <meshBasicMaterial color={hovered ? 0xff0000 : 0x00ff00} wireframe />
     </mesh>
   )
 }
