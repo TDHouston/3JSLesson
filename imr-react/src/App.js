74cf3da26d3bcf98fa89efe8f565ee3d4151414b
TDHouston <trentdhouston@gmail.com>
2023-08-06 19:30:02 -0400

    UseGLTF example

--- imr-react/src/App.js
+++ imr-react/src/App.js
@@ -1,18 +1,37 @@
-import { Stats, OrbitControls, Environment } from '@react-three/drei'
-import { Canvas, useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Stats, OrbitControls, Environment, useGLTF, Clone } from '@react-three/drei'
+import { useControls } from 'leva'
+
+const Models = [
+  { title: 'Hammer', url: './models/hammer.glb' },
+  { title: 'Drill', url: './models/drill.glb' },
+  { title: 'Tape Measure', url: './models/tapeMeasure.glb' },
+]
+
+function Model({ url }) {
+  const { scene } = useGLTF(url)
+  return <Clone object={scene} />
+}
 
 export function App() {
-  const gltf = useLoader(GLTFLoader, './models/monkey.glb')
+  const { title } = useControls({
+    title: {
+      options: Models.map(({ title }) => title),
+    },
+  })
 
   return (
-    <Canvas camera={{ position: [-0.5, 1, 2] }}>
-      <Environment files="./img/venice_sunset_1k.hdr" background blur={0.5} />
-      <directionalLight position={[3.3, 1.0, 4.4]} intensity={4} />
-      <primitive object={gltf.scene} position={[0, 1, 0]} />
-      <OrbitControls target={[0, 1, 0]} autoRotate />
-      <axesHelper args={[5]} />
-      <Stats />
-    </Canvas>
+    <>
+      <Canvas camera={{ position: [0, 0, -0.2], near: 0.025 }}>
+        <Environment files="./img/workshop_1k.hdr" background />
+        <Suspense>
+          <Model url={Models[Models.findIndex((m) => m.title === title)].url} />
+        </Suspense>
+        <OrbitControls autoRotate />
+        <Stats />
+      </Canvas>
+      <span id="info">The {title} is selected.</span>
+    </>
   )
 }
