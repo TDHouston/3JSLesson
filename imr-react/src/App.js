b16a850d3feaf4187a6bd933231307d1c5b20f65
TDHouston <trentdhouston@gmail.com>
2023-08-08 20:42:30 -0400

    misc. updates

--- imr-react/src/App.js
+++ imr-react/src/App.js
@@ -1,19 +1,20 @@
-import { Canvas } from '@react-three/fiber'
-import Box from './Box'
-import { Stats, OrbitControls } from '@react-three/drei'
-import useKeyboard from './useKeyboard'
+import { Stats, OrbitControls, Environment } from '@react-three/drei'
+import { Canvas, useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 export default function App() {
-  const keyMap = useKeyboard()
+  const gltf = useLoader(GLTFLoader, './models/inMyRoom.glb')
 
   return (
-    <Canvas camera={{ position: [1, 2, 3] }}>
-      <Box position={[-1.5, 0.5, 0]} keyMap={keyMap} />
-      <Box position={[0, 0.5, 0]} keyMap={keyMap} selected />
-      <Box position={[1.5, 0.5, 0]} keyMap={keyMap} />
-      <OrbitControls />
+    <Canvas>
+      <Environment preset='apartment' background blur={0.5}/>
+      <primitive
+        object={gltf.scene}
+        position={[0, 1, 0]}
+        children-0-castShadow
+      />
+      <OrbitControls target={[0, 1, 0]} />
       <axesHelper args={[5]} />
-      <gridHelper />
       <Stats />
     </Canvas>
   )
